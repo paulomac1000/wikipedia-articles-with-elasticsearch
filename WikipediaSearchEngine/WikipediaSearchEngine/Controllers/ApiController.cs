7bee9206e7f5087427525946ea44fa219ddb2761
Paweł Maciejewski <paulomac1000@gmail.com>
2020-02-11 02:50:25 +0100

    create web project to manage elastic data with basic api for handle elasticsearch data

---
+++ WikipediaSearchEngine/WikipediaSearchEngine/Controllers/ApiController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using WikipediaSearchEngine.Models;
+
+namespace WikipediaSearchEngine.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApiController : Controller
+    {
+        private readonly IElasticClient _elasticClient;
+
+        public ApiController(IElasticClient elasticClient)
+        {
+            _elasticClient = elasticClient;
+        }
+
+        [HttpGet("simple-query-string")]
+        public async Task<IActionResult> SimpleQueryString(string term)
+        {
+            var res = await _elasticClient.SearchAsync<Document>(x => x
+                .Query(q => q.SimpleQueryString(qs => qs.Query(term))));
+
+            var res2 = await _elasticClient.SearchAsync<Document>(x => x
+                .Query(q => q.CommonTerms(qs => qs.Query(term))));
+
+            if (!res.IsValid)
+                throw new InvalidOperationException(res.DebugInformation);
+
+            return Json(res.Documents);
+        }
+
+        [HttpGet("common-terms")]
+        public async Task<IActionResult> CommonTerms(string term)
+        {
+            var res = await _elasticClient.SearchAsync<Document>(x => x
+                .Query(q => q.CommonTerms(qs => qs.Query(term))));
+
+            if (!res.IsValid)
+                throw new InvalidOperationException(res.DebugInformation);
+
+            return Json(res.Documents);
+        }
+    }
+}
---
+++ WikipediaSearchEngine/WikipediaSearchEngine/Startup.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nest;
+using System;
+using Microsoft.OpenApi.Models;
+using WikipediaSearchEngine.Models;
+
+namespace WikipediaSearchEngine
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            RegisterElasticsearch(services);
+            RegisterSwagger(services);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Wikipedia Search Engine Api");
+            });
+        }
+
+        private void RegisterElasticsearch(IServiceCollection services)
+        {
+            var url = Configuration["Elastic:Url"];
+            var defaultIndex = Configuration["Elastic:Index"];
+
+            var settings = new ConnectionSettings(new Uri(url))
+                .DefaultIndex(defaultIndex)
+                .DefaultMappingFor<Document>(m => m);
+
+            var client = new ElasticClient(settings);
+
+            services.AddSingleton<IElasticClient>(client);
+        }
+
+        private static void RegisterSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Wikipedia Search Engine Api",
+                    Description = "Api examples"
+                });
+            });
+        }
+    }
+}
